2f3c7a600d740c8ad118f6df9a49294d586ab845
dphansti <douglas_phanstiel@med.unc.edu>
2017-09-28 17:36:15 -0400

    Added manual page info

--- R/bedtools.R
+++ R/bedtools.R
@@ -1,8 +1,34 @@
+#' performs bedtools intersections
+#'
+#' Bedtools needs to be in your system PATH
+#'
+#' @param action the bedtools function to be used (Default=intersect)
+#' @param args any arguments to be added (e.g. -wo) (Default=-wo)
+#' @param a a filename or data frame to be used a set A
+#' @param b a filename or data frame to be used a set B
+#' @param acols which columns from set A to use
+#' @param bcols which columns from set B to use
+#' @param ahead boolean whether set A has a header (only relevant for is a is a filename) (Default=F)
+#' @param bhead boolean whether set B has a header (only relevant for is b is a filename) (Default=F)
+#' @param count boolean whether to return the results of the intersection or just a line count of the result
+#' @param tmpdir inset from the top of the blot as a fraction of the plot width
+#' @param columnfilt The number of columns to return
+#' @param test boolean, if TRUE the function will print out the bedtools command but not execute it
+#' @export
 
 
 bedtools <- function(action="intersect",args="-wo",a,b,acols=NA,bcols=NA,ahead=FALSE,bhead=FALSE,count=FALSE,tmpdir="~/scratch/",columnfilt = NA,test=FALSE)
 {
-  options(scipen=999) 
+  # interior function to delete temp files
+  deletethefiles <- function()
+  {
+    # delete temp files
+    if (file.exists(afile)){file.remove(afile)}
+    if (file.exists(bfile)){file.remove(bfile)}
+    if (file.exists(cfile)){file.remove(cfile)}
+  }
+  
+   options(scipen=999) 
   
   # file names
   afile = paste(tmpdir,"tmpa.bed",sep="")
@@ -63,14 +89,16 @@ bedtools <- function(action="intersect",args="-wo",a,b,acols=NA,bcols=NA,ahead=F
     {
       if (count == TRUE)
       {
+        deletethefiles()
         return (0)
       }
+      deletethefiles()
       return(c())
     }
     if (info$size > 0)
     {
       res = read.table(cfile,header=F,sep="\t")
-      
+      deletethefiles()
       if (count == TRUE)
       {
         return (nrow(res))
@@ -79,3 +107,6 @@ bedtools <- function(action="intersect",args="-wo",a,b,acols=NA,bcols=NA,ahead=F
     }
   }
 }
+
+
+
